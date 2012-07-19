dad46e950c7c2e3bacda2f8d1c1176e20795294d
guoyin.chen <guoyin.chen@freescale.com>
2012-07-19 13:00:57 +0800

    ENGR00221840 add YPbPr format support
    Change-Id: Icca6dbd6565799cb9e6cdfb5f511063ddfcb3910
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/399
    Reviewed-by: Xiaowen Liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/870

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -441,6 +441,10 @@ typedef enum android_pixel_format {
     HAL_PIXEL_FORMAT_YCbCr_422_SP       = 0x10, // NV16
     HAL_PIXEL_FORMAT_YCrCb_420_SP       = 0x11, // NV21
     HAL_PIXEL_FORMAT_YCbCr_422_I        = 0x14, // YUY2
+    HAL_PIXEL_FORMAT_YCbCr_422_P        = 0x100,
+    HAL_PIXEL_FORMAT_YCbCr_420_P        = 0x101,
+    HAL_PIXEL_FORMAT_CbYCrY_422_I       = 0x102,
+    HAL_PIXEL_FORMAT_YCbCr_420_SP       = 0x103,
 } android_pixel_format_t;
 
 /*
